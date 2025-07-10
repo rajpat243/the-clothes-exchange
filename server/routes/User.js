4fe0c93ee41f0dd991645b89016824d3e1f39bd2
Alice Smith <alice@webage.com>
2025-07-10 13:07:46 -0400

    Merged with main branch

--- server/routes/User.js
+++ server/routes/User.js
@@ -19,3 +19,30 @@ returns: Name, Email
 
 */
 
+import express from 'express';
+import { MongoClient, ObjectId } from 'mongodb';
+const userRouter = new express.Router()
+const url = 'mongodb://localhost:27017';
+const dbName = 'clothing_store';
+const collectionName = 'product';
+
+
+const getCollection = async () => {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    return db.collection(collectionName);
+};
+
+userRouter.get('/api/product', async (req, res) => {
+    try {
+        const productCollection = await getCollection();
+        const products = await productCollection.find({}).toArray();
+        
+        res.json(products);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Getting product failed!");
+    }
+});
+
+export default userRouter;
